cb569a2a22f902fb629972f40243d176efba79bd
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2024-05-15 06:40:23 -0400

    fix border/box sizing error

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 const navbar = () => {
   return (
     <nav className='bg-zinc-800 text-neutral-100 w-screen p-4 flex flex-row items-center justify-end'>
-      <Link className='hover:border-b-2 mx-2 text-sm font-font-semibold tracking-widest uppercase' href={'/'}>Home</Link>
-      <Link className='hover:border-b-2 mx-2 text-sm font-font-semibold tracking-widest uppercase' href={'/about'}>About</Link>
-      <Link className='hover:border-b-2 mx-2 text-sm font-font-semibold tracking-widest uppercase' href={'/portfolio'}>Portfolio</Link>
-      <Link className='hover:border-b-2 mx-2 text-sm font-font-semibold tracking-widest uppercase' href={'/contact'}>Contact</Link>
+      <Link className='hover:underline mx-2 text-sm font-font-semibold tracking-widest uppercase' href={'/'}>Home</Link>
+      <Link className='hover:underline mx-2 text-sm font-font-semibold tracking-widest uppercase' href={'/about'}>About</Link>
+      <Link className='hover:underline mx-2 text-sm font-font-semibold tracking-widest uppercase' href={'/portfolio'}>Portfolio</Link>
+      <Link className='hover:underline mx-2 text-sm font-font-semibold tracking-widest uppercase' href={'/contact'}>Contact</Link>
     </nav>
   )
 }
