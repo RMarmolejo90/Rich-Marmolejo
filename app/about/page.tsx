8add9e6f2c163eb308d733157367be37b3763ad7
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2024-05-15 10:43:47 -0400

    updated the about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,10 @@ const About = () => {
   return (
     <div className='p-4 mx-4 lg:mx-20 md:mx-10 flex flex-col items-center justify-center'> 
       <h1 className='md:p-10 p-4 font-bold text-2xl'>Full Stack Software Developer</h1>
-      <p className='md:p-10 p-4'>I genuinely love writing great software. After taking the IBM Full Stack Developer course where I gained some insight into the world of microservices and containerization I changed as a developer. I find myself now focusing more on writing maintainable, modular, scalable code that supports User Experience and development lifecycles.</p>
+      <p className='md:p-10 p-4'>I genuinely love writing great software. After taking the IBM Full Stack Developer course, I gained some insight into the world of microservices and containerization. Since then, I find myself focused on writing maintainable, modular, scalable code that supports User Experience and development lifecycles.</p>
+
+      <p className='md:p-10 p-4'>I studied the Harvard CS50 course material to gain a deeper understanding of computer science, and how computers operate at a low level. I was so motivated by the course material, that I began to study languages like GoLang, Rust, C#, and C++. It really is very interesting how these different technologies overcome certain issues in their own unique style. What they have in common is that they are all so purposeful, and dynamic in their own way. So as a developer, I intend to be purposeful and dynamic in the way I write my code. That means my code should be direct, but fluid and adaptable, as to work well with the rest of the code base, work well with a team, and resolve issues with direct purpose and simplicity. At the core, that's what I'm all about as a software developer.
+      </p>
 
       <div className='lg:p-10 p-2 flex flex-row flex-wrap items-center justify-center text-xl font-semibold'>
         <h2 className='lg:m-4 m-2'>React</h2>
